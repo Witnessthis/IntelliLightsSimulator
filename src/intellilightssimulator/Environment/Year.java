a20ab8067fc168389e33c8e975d2b4e56666a213
Nanna Dohn <nj_kd@hotmail.com>
2016-11-18 19:30:16 +0100

    Year.java file added to project

---
+++ src/intellilightssimulator/Environment/Year.java
@@ -0,0 +1,76 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package intellilightssimulator.Environment;
+
+/**
+ *
+ * @author Nanna Dohn
+ */
+public class Year { 
+
+    public static float jan;
+    public static float feb;
+    public static float mar;
+    public static float apr;
+    public static float may;
+    public static float jun;
+    public static float jul;
+    public static float aug;
+    public static float sep;
+    public static float oct;
+    public static float nov;
+    public static float dec;
+
+    public static float getJan() {
+        return jan;
+    }
+
+    public static float getFeb() {
+        return feb;
+    }
+
+    public static float getMar() {
+        return mar;
+    }
+
+    public static float getApr() {
+        return apr;
+    }
+
+    public static float getMay() {
+        return may;
+    }
+
+    public static float getJun() {
+        return jun;
+    }
+
+    public static float getJul() {
+        return jul;
+    }
+
+    public static float getAug() {
+        return aug;
+    }
+
+    public static float getSep() {
+        return sep;
+    }
+
+    public static float getOct() {
+        return oct;
+    }
+
+    public static float getNov() {
+        return nov;
+    }
+
+    public static float getDec() {
+        return dec;
+    }
+    
+    
+}
